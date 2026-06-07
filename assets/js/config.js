be090a4c18de5e0a948d26050758a1880a76c83d
lfccrafting <lfc.crafting@gmail.com>
2026-06-07 22:59:09 +0200

    Update config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -2,5 +2,5 @@
 // WICHTIG: Hier nur SUPABASE ANON KEY eintragen, niemals service_role key.
 window.LFC_SUPABASE_CONFIG = {
   url: "https://mwyaoijpvwjvutmctqxv.supabase.co",
-  anonKey: "HIER_SUPABASE_ANON_KEY_EINTRAGEN"
+  anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im13eWFvaWpwdndqdnV0bWN0cXh2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODA4NTkyMjMsImV4cCI6MjA5NjQzNTIyM30.TgRpc5YT56M9cpQxeichZWHbUF-eTdAlC6PbDgYvjgA"
 };
